0c8766e4ca4efc1ae0eff53a81845ebf69397fca
davidcoats <david@getsafetycone.com>
2023-06-11 14:03:05 -0700

    Initial commit.

---
+++ source/R5T.S0078/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0078
+{
+	/// <summary>
+	/// Roslyn workspace scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.S0078/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.S0078
+Roslyn workspace scripts.
